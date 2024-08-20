dcaaf38e4fe438225de443e88724b15065f782b7
maro911220 <kingdomboze@gmail.com>
2024-08-20 21:47:02 +0900

    api test

---
+++ src/types/card.ts
@@ -0,0 +1,11 @@
+export interface cardTypes {
+  atk: string;
+  def: string;
+  name: string;
+  card_images: { image_url: string }[];
+  type: string;
+  level: number;
+  attribute: string;
+  desc: string;
+  race: string;
+}
